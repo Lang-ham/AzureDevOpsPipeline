36b3824e8232d29f1c83886705ec3ed18ef03c28
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-01-07 20:12:33 +0000

    Merging fixes and enhancements into trunk.

--- wordpress/wp-admin/includes/network.php
+++ wordpress/wp-admin/includes/network.php
@@ -266,4 +266,260 @@ function network_step1( $errors = false ) {
 		<table class="form-table">
 		<?php if ( 'localhost' == $hostname ) : ?>
 			<tr>
-				<th scope="row"><?php esc_html_e( 'Sub-directory Installati
+				<th scope="row"><?php esc_html_e( 'Sub-directory Installation' ); ?></th>
+				<td><?php
+					printf(
+						/* translators: 1: localhost 2: localhost.localdomain */
+						__( 'Because you are using %1$s, the sites in your WordPress network must use sub-directories. Consider using %2$s if you wish to use sub-domains.' ),
+						'<code>localhost</code>',
+						'<code>localhost.localdomain</code>'
+					);
+					// Uh oh:
+					if ( !allow_subdirectory_install() )
+						echo ' <strong>' . __( 'Warning:' ) . ' ' . __( 'The main site in a sub-directory installation will need to use a modified permalink structure, potentially breaking existing links.' ) . '</strong>';
+				?></td>
+			</tr>
+		<?php elseif ( !allow_subdomain_install() ) : ?>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Sub-directory Installation' ); ?></th>
+				<td><?php
+					_e( 'Because your installation is in a directory, the sites in your WordPress network must use sub-directories.' );
+					// Uh oh:
+					if ( !allow_subdirectory_install() )
+						echo ' <strong>' . __( 'Warning:' ) . ' ' . __( 'The main site in a sub-directory installation will need to use a modified permalink structure, potentially breaking existing links.' ) . '</strong>';
+				?></td>
+			</tr>
+		<?php elseif ( !allow_subdirectory_install() ) : ?>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Sub-domain Installation' ); ?></th>
+				<td><?php _e( 'Because your installation is not new, the sites in your WordPress network must use sub-domains.' );
+					echo ' <strong>' . __( 'The main site in a sub-directory installation will need to use a modified permalink structure, potentially breaking existing links.' ) . '</strong>';
+				?></td>
+			</tr>
+		<?php endif; ?>
+		<?php if ( ! $is_www ) : ?>
+			<tr>
+				<th scope='row'><?php esc_html_e( 'Server Address' ); ?></th>
+				<td>
+					<?php printf(
+						/* translators: %s: host name */
+						__( 'The internet address of your network will be %s.' ),
+						'<code>' . $hostname . '</code>'
+					); ?>
+				</td>
+			</tr>
+		<?php endif; ?>
+			<tr>
+				<th scope='row'><?php esc_html_e( 'Network Title' ); ?></th>
+				<td>
+					<input name='sitename' type='text' size='45' value='<?php echo esc_attr( $site_name ); ?>' />
+					<p class="description">
+						<?php _e( 'What would you like to call your network?' ); ?>
+					</p>
+				</td>
+			</tr>
+			<tr>
+				<th scope='row'><?php esc_html_e( 'Network Admin Email' ); ?></th>
+				<td>
+					<input name='email' type='text' size='45' value='<?php echo esc_attr( $admin_email ); ?>' />
+					<p class="description">
+						<?php _e( 'Your email address.' ); ?>
+					</p>
+				</td>
+			</tr>
+		</table>
+		<?php submit_button( __( 'Install' ), 'primary', 'submit' ); ?>
+	</form>
+	<?php
+}
+
+/**
+ * Prints step 2 for Network installation process.
+ *
+ * @since 3.0.0
+ *
+ * @global wpdb $wpdb WordPress database abstraction object.
+ *
+ * @param WP_Error $errors
+ */
+function network_step2( $errors = false ) {
+	global $wpdb;
+
+	$hostname          = get_clean_basedomain();
+	$slashed_home      = trailingslashit( get_option( 'home' ) );
+	$base              = parse_url( $slashed_home, PHP_URL_PATH );
+	$document_root_fix = str_replace( '\\', '/', realpath( $_SERVER['DOCUMENT_ROOT'] ) );
+	$abspath_fix       = str_replace( '\\', '/', ABSPATH );
+	$home_path         = 0 === strpos( $abspath_fix, $document_root_fix ) ? $document_root_fix . $base : get_home_path();
+	$wp_siteurl_subdir = preg_replace( '#^' . preg_quote( $home_path, '#' ) . '#', '', $abspath_fix );
+	$rewrite_base      = ! empty( $wp_siteurl_subdir ) ? ltrim( trailingslashit( $wp_siteurl_subdir ), '/' ) : '';
+
+
+	$location_of_wp_config = $abspath_fix;
+	if ( ! file_exists( ABSPATH . 'wp-config.php' ) && file_exists( dirname( ABSPATH ) . '/wp-config.php' ) ) {
+		$location_of_wp_config = dirname( $abspath_fix );
+	}
+	$location_of_wp_config = trailingslashit( $location_of_wp_config );
+
+	// Wildcard DNS message.
+	if ( is_wp_error( $errors ) )
+		echo '<div class="error">' . $errors->get_error_message() . '</div>';
+
+	if ( $_POST ) {
+		if ( allow_subdomain_install() )
+			$subdomain_install = allow_subdirectory_install() ? ! empty( $_POST['subdomain_install'] ) : true;
+		else
+			$subdomain_install = false;
+	} else {
+		if ( is_multisite() ) {
+			$subdomain_install = is_subdomain_install();
+?>
+	<p><?php _e( 'The original configuration steps are shown here for reference.' ); ?></p>
+<?php
+		} else {
+			$subdomain_install = (bool) $wpdb->get_var( "SELECT meta_value FROM $wpdb->sitemeta WHERE site_id = 1 AND meta_key = 'subdomain_install'" );
+?>
+	<div class="error"><p><strong><?php _e( 'Warning:' ); ?></strong> <?php _e( 'An existing WordPress network was detected.' ); ?></p></div>
+	<p><?php _e( 'Please complete the configuration steps. To create a new network, you will need to empty or remove the network database tables.' ); ?></p>
+<?php
+		}
+	}
+
+	$subdir_match          = $subdomain_install ? '' : '([_0-9a-zA-Z-]+/)?';
+	$subdir_replacement_01 = $subdomain_install ? '' : '$1';
+	$subdir_replacement_12 = $subdomain_install ? '$1' : '$2';
+
+	if ( $_POST || ! is_multisite() ) {
+?>
+		<h3><?php esc_html_e( 'Enabling the Network' ); ?></h3>
+		<p><?php _e( 'Complete the following steps to enable the features for creating a network of sites.' ); ?></p>
+		<div class="updated inline"><p><?php
+			if ( file_exists( $home_path . '.htaccess' ) ) {
+				echo '<strong>' . __( 'Caution:' ) . '</strong> ';
+				printf(
+					/* translators: 1: wp-config.php 2: .htaccess */
+					__( 'We recommend you back up your existing %1$s and %2$s files.' ),
+					'<code>wp-config.php</code>',
+					'<code>.htaccess</code>'
+				);
+			} elseif ( file_exists( $home_path . 'web.config' ) ) {
+				echo '<strong>' . __( 'Caution:' ) . '</strong> ';
+				printf(
+					/* translators: 1: wp-config.php 2: web.config */
+					__( 'We recommend you back up your existing %1$s and %2$s files.' ),
+					'<code>wp-config.php</code>',
+					'<code>web.config</code>'
+				);
+			} else {
+				echo '<strong>' . __( 'Caution:' ) . '</strong> ';
+				printf(
+					/* translators: 1: wp-config.php */
+					__( 'We recommend you back up your existing %s file.' ),
+					'<code>wp-config.php</code>'
+				);
+			}
+		?></p></div>
+<?php
+	}
+?>
+		<ol>
+			<li><p><?php printf(
+				/* translators: 1: wp-config.php 2: location of wp-config file, 3: translated version of "That's all, stop editing! Happy blogging." */
+				__( 'Add the following to your %1$s file in %2$s <strong>above</strong> the line reading %3$s:' ),
+				'<code>wp-config.php</code>',
+				'<code>' . $location_of_wp_config . '</code>',
+				/*
+				 * translators: This string should only be translated if wp-config-sample.php is localized.
+				 * You can check the localized release package or
+				 * https://i18n.svn.wordpress.org/<locale code>/branches/<wp version>/dist/wp-config-sample.php
+				 */
+				'<code>/* ' . __( 'That&#8217;s all, stop editing! Happy blogging.' ) . ' */</code>'
+			); ?></p>
+				<textarea class="code" readonly="readonly" cols="100" rows="7">
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', <?php echo $subdomain_install ? 'true' : 'false'; ?>);
+define('DOMAIN_CURRENT_SITE', '<?php echo $hostname; ?>');
+define('PATH_CURRENT_SITE', '<?php echo $base; ?>');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+</textarea>
+<?php
+	$keys_salts = array( 'AUTH_KEY' => '', 'SECURE_AUTH_KEY' => '', 'LOGGED_IN_KEY' => '', 'NONCE_KEY' => '', 'AUTH_SALT' => '', 'SECURE_AUTH_SALT' => '', 'LOGGED_IN_SALT' => '', 'NONCE_SALT' => '' );
+	foreach ( $keys_salts as $c => $v ) {
+		if ( defined( $c ) )
+			unset( $keys_salts[ $c ] );
+	}
+
+	if ( ! empty( $keys_salts ) ) {
+		$keys_salts_str = '';
+		$from_api = wp_remote_get( 'https://api.wordpress.org/secret-key/1.1/salt/' );
+		if ( is_wp_error( $from_api ) ) {
+			foreach ( $keys_salts as $c => $v ) {
+				$keys_salts_str .= "\ndefine( '$c', '" . wp_generate_password( 64, true, true ) . "' );";
+			}
+		} else {
+			$from_api = explode( "\n", wp_remote_retrieve_body( $from_api ) );
+			foreach ( $keys_salts as $c => $v ) {
+				$keys_salts_str .= "\ndefine( '$c', '" . substr( array_shift( $from_api ), 28, 64 ) . "' );";
+			}
+		}
+		$num_keys_salts = count( $keys_salts );
+?>
+	<p>
+		<?php
+			if ( 1 == $num_keys_salts ) {
+				printf(
+					/* translators: 1: wp-config.php */
+					__( 'This unique authentication key is also missing from your %s file.' ),
+					'<code>wp-config.php</code>'
+				);
+			} else {
+				printf(
+					/* translators: 1: wp-config.php */
+					__( 'These unique authentication keys are also missing from your %s file.' ),
+					'<code>wp-config.php</code>'
+				);
+			}
+		?>
+		<?php _e( 'To make your installation more secure, you should also add:' ); ?>
+	</p>
+	<textarea class="code" readonly="readonly" cols="100" rows="<?php echo $num_keys_salts; ?>"><?php echo esc_textarea( $keys_salts_str ); ?></textarea>
+<?php
+	}
+?>
+</li>
+<?php
+	if ( iis7_supports_permalinks() ) :
+		// IIS doesn't support RewriteBase, all your RewriteBase are belong to us
+		$iis_subdir_match = ltrim( $base, '/' ) . $subdir_match;
+		$iis_rewrite_base = ltrim( $base, '/' ) . $rewrite_base;
+		$iis_subdir_replacement = $subdomain_install ? '' : '{R:1}';
+
+		$web_config_file = '<?xml version="1.0" encoding="UTF-8"?>
+<configuration>
+    <system.webServer>
+        <rewrite>
+            <rules>
+                <rule name="WordPress Rule 1" stopProcessing="true">
+                    <match url="^index\.php$" ignoreCase="false" />
+                    <action type="None" />
+                </rule>';
+				if ( is_multisite() && get_site_option( 'ms_files_rewriting' ) ) {
+					$web_config_file .= '
+                <rule name="WordPress Rule for Files" stopProcessing="true">
+                    <match url="^' . $iis_subdir_match . 'files/(.+)" ignoreCase="false" />
+                    <action type="Rewrite" url="' . $iis_rewrite_base . WPINC . '/ms-files.php?file={R:1}" appendQueryString="false" />
+                </rule>';
+                }
+                $web_config_file .= '
+                <rule name="WordPress Rule 2" stopProcessing="true">
+                    <match url="^' . $iis_subdir_match . 'wp-admin$" ignoreCase="false" />
+                    <action type="Redirect" url="' . $iis_subdir_replacement . 'wp-admin/" redirectType="Permanent" />
+                </rule>
+                <rule name="WordPress Rule 3" stopProcessing="true">
+                    <match url="^" ignoreCase="false" />
+                    <conditions logicalGrouping="MatchAny">
+                        <add input="{REQUEST_FILENAME}" matchType="IsFile" ignoreCase="false" />
+                        <add input="{REQUEST_FILENAME}" matchType="IsDirectory" ignoreCase="false" />
+                    </conditions>
+               
