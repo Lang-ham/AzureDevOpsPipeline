2f32b90a438f02507cd8ca79a0a45a802fa41533
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-01-16 22:33:51 +0000

    From Valeriy Onuchin:

--- wordpress/wp-admin/includes/plugin-install.php
+++ wordpress/wp-admin/includes/plugin-install.php
@@ -36,4 +36,232 @@
  * | `$number`            | No            |  No                | Yes      | Yes            |
  * | `$search`            | Yes           |  No                | No       | No             |
  * | `$tag`               | Yes           |  No                | No       | No             |
- * | `$author`            | Yes           |  No                | No       | No             
+ * | `$author`            | Yes           |  No                | No       | No             |
+ * | `$user`              | Yes           |  No                | No       | No             |
+ * | `$browse`            | Yes           |  No                | No       | No             |
+ * | `$locale`            | Yes           |  Yes               | No       | No             |
+ * | `$installed_plugins` | Yes           |  No                | No       | No             |
+ * | `$is_ssl`            | Yes           |  Yes               | No       | No             |
+ * | `$fields`            | Yes           |  Yes               | No       | No             |
+ *
+ * @since 2.7.0
+ *
+ * @param string       $action API action to perform: 'query_plugins', 'plugin_information',
+ *                             'hot_tags' or 'hot_categories'.
+ * @param array|object $args   {
+ *     Optional. Array or object of arguments to serialize for the Plugin Info API.
+ *
+ *     @type string  $slug              The plugin slug. Default empty.
+ *     @type int     $per_page          Number of plugins per page. Default 24.
+ *     @type int     $page              Number of current page. Default 1.
+ *     @type int     $number            Number of tags or categories to be queried.
+ *     @type string  $search            A search term. Default empty.
+ *     @type string  $tag               Tag to filter plugins. Default empty.
+ *     @type string  $author            Username of an plugin author to filter plugins. Default empty.
+ *     @type string  $user              Username to query for their favorites. Default empty.
+ *     @type string  $browse            Browse view: 'popular', 'new', 'beta', 'recommended'.
+ *     @type string  $locale            Locale to provide context-sensitive results. Default is the value
+ *                                      of get_locale().
+ *     @type string  $installed_plugins Installed plugins to provide context-sensitive results.
+ *     @type bool    $is_ssl            Whether links should be returned with https or not. Default false.
+ *     @type array   $fields            {
+ *         Array of fields which should or should not be returned.
+ *
+ *         @type bool $short_description Whether to return the plugin short description. Default true.
+ *         @type bool $description       Whether to return the plugin full description. Default false.
+ *         @type bool $sections          Whether to return the plugin readme sections: description, installation,
+ *                                       FAQ, screenshots, other notes, and changelog. Default false.
+ *         @type bool $tested            Whether to return the 'Compatible up to' value. Default true.
+ *         @type bool $requires          Whether to return the required WordPress version. Default true.
+ *         @type bool $rating            Whether to return the rating in percent and total number of ratings.
+ *                                       Default true.
+ *         @type bool $ratings           Whether to return the number of rating for each star (1-5). Default true.
+ *         @type bool $downloaded        Whether to return the download count. Default true.
+ *         @type bool $downloadlink      Whether to return the download link for the package. Default true.
+ *         @type bool $last_updated      Whether to return the date of the last update. Default true.
+ *         @type bool $added             Whether to return the date when the plugin was added to the wordpress.org
+ *                                       repository. Default true.
+ *         @type bool $tags              Whether to return the assigned tags. Default true.
+ *         @type bool $compatibility     Whether to return the WordPress compatibility list. Default true.
+ *         @type bool $homepage          Whether to return the plugin homepage link. Default true.
+ *         @type bool $versions          Whether to return the list of all available versions. Default false.
+ *         @type bool $donate_link       Whether to return the donation link. Default true.
+ *         @type bool $reviews           Whether to return the plugin reviews. Default false.
+ *         @type bool $banners           Whether to return the banner images links. Default false.
+ *         @type bool $icons             Whether to return the icon links. Default false.
+ *         @type bool $active_installs   Whether to return the number of active installations. Default false.
+ *         @type bool $group             Whether to return the assigned group. Default false.
+ *         @type bool $contributors      Whether to return the list of contributors. Default false.
+ *     }
+ * }
+ * @return object|array|WP_Error Response object or array on success, WP_Error on failure. See the
+ *         {@link https://developer.wordpress.org/reference/functions/plugins_api/ function reference article}
+ *         for more information on the make-up of possible return values depending on the value of `$action`.
+ */
+function plugins_api( $action, $args = array() ) {
+
+	if ( is_array( $args ) ) {
+		$args = (object) $args;
+	}
+
+	if ( ! isset( $args->per_page ) ) {
+		$args->per_page = 24;
+	}
+
+	if ( ! isset( $args->locale ) ) {
+		$args->locale = get_user_locale();
+	}
+
+	/**
+	 * Filters the WordPress.org Plugin Installation API arguments.
+	 *
+	 * Important: An object MUST be returned to this filter.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param object $args   Plugin API arguments.
+	 * @param string $action The type of information being requested from the Plugin Installation API.
+	 */
+	$args = apply_filters( 'plugins_api_args', $args, $action );
+
+	/**
+	 * Filters the response for the current WordPress.org Plugin Installation API request.
+	 *
+	 * Passing a non-false value will effectively short-circuit the WordPress.org API request.
+	 *
+	 * If `$action` is 'query_plugins' or 'plugin_information', an object MUST be passed.
+	 * If `$action` is 'hot_tags' or 'hot_categories', an array should be passed.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param false|object|array $result The result object or array. Default false.
+	 * @param string             $action The type of information being requested from the Plugin Installation API.
+	 * @param object             $args   Plugin API arguments.
+	 */
+	$res = apply_filters( 'plugins_api', false, $action, $args );
+
+	if ( false === $res ) {
+		// include an unmodified $wp_version
+		include( ABSPATH . WPINC . '/version.php' );
+
+		$url = $http_url = 'http://api.wordpress.org/plugins/info/1.0/';
+		if ( $ssl = wp_http_supports( array( 'ssl' ) ) )
+			$url = set_url_scheme( $url, 'https' );
+
+		$http_args = array(
+			'timeout' => 15,
+			'user-agent' => 'WordPress/' . $wp_version . '; ' . home_url( '/' ),
+			'body' => array(
+				'action' => $action,
+				'request' => serialize( $args )
+			)
+		);
+		$request = wp_remote_post( $url, $http_args );
+
+		if ( $ssl && is_wp_error( $request ) ) {
+			trigger_error(
+				sprintf(
+					/* translators: %s: support forums URL */
+					__( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="%s">support forums</a>.' ),
+					__( 'https://wordpress.org/support/' )
+				) . ' ' . __( '(WordPress could not establish a secure connection to WordPress.org. Please contact your server administrator.)' ),
+				headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE
+			);
+			$request = wp_remote_post( $http_url, $http_args );
+		}
+
+		if ( is_wp_error($request) ) {
+			$res = new WP_Error( 'plugins_api_failed',
+				sprintf(
+					/* translators: %s: support forums URL */
+					__( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="%s">support forums</a>.' ),
+					__( 'https://wordpress.org/support/' )
+				),
+				$request->get_error_message()
+			);
+		} else {
+			$res = maybe_unserialize( wp_remote_retrieve_body( $request ) );
+			if ( ! is_object( $res ) && ! is_array( $res ) ) {
+				$res = new WP_Error( 'plugins_api_failed',
+					sprintf(
+						/* translators: %s: support forums URL */
+						__( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="%s">support forums</a>.' ),
+						__( 'https://wordpress.org/support/' )
+					),
+					wp_remote_retrieve_body( $request )
+				);
+			}
+		}
+	} elseif ( !is_wp_error($res) ) {
+		$res->external = true;
+	}
+
+	/**
+	 * Filters the Plugin Installation API response results.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param object|WP_Error $res    Response object or WP_Error.
+	 * @param string          $action The type of information being requested from the Plugin Installation API.
+	 * @param object          $args   Plugin API arguments.
+	 */
+	return apply_filters( 'plugins_api_result', $res, $action, $args );
+}
+
+/**
+ * Retrieve popular WordPress plugin tags.
+ *
+ * @since 2.7.0
+ *
+ * @param array $args
+ * @return array
+ */
+function install_popular_tags( $args = array() ) {
+	$key = md5(serialize($args));
+	if ( false !== ($tags = get_site_transient('poptags_' . $key) ) )
+		return $tags;
+
+	$tags = plugins_api('hot_tags', $args);
+
+	if ( is_wp_error($tags) )
+		return $tags;
+
+	set_site_transient( 'poptags_' . $key, $tags, 3 * HOUR_IN_SECONDS );
+
+	return $tags;
+}
+
+/**
+ * @since 2.7.0
+ */
+function install_dashboard() {
+	?>
+	<p><?php printf( __( 'Plugins extend and expand the functionality of WordPress. You may automatically install plugins from the <a href="%1$s">WordPress Plugin Directory</a> or upload a plugin in .zip format by clicking the button at the top of this page.' ), __( 'https://wordpress.org/plugins/' ) ); ?></p>
+
+	<?php display_plugins_table(); ?>
+
+	<div class="plugins-popular-tags-wrapper">
+	<h2><?php _e( 'Popular tags' ) ?></h2>
+	<p><?php _e( 'You may also browse based on the most popular tags in the Plugin Directory:' ) ?></p>
+	<?php
+
+	$api_tags = install_popular_tags();
+
+	echo '<p class="popular-tags">';
+	if ( is_wp_error($api_tags) ) {
+		echo $api_tags->get_error_message();
+	} else {
+		//Set up the tags in a way which can be interpreted by wp_generate_tag_cloud()
+		$tags = array();
+		foreach ( (array) $api_tags as $tag ) {
+			$url = self_admin_url( 'plugin-install.php?tab=search&type=tag&s=' . urlencode( $tag['name'] ) );
+			$data = array(
+				'link' => esc_url( $url ),
+				'name' => $tag['name'],
+				'slug' => $tag['slug'],
+				'id' => sanitize_title_with_dashes( $tag['name'] ),
+				'count' => $tag['count']
+			);
+			$tags[ $tag['name'] ] = (object) $data;
+		}
+		echo wp_generate_tag_cloud($tag
