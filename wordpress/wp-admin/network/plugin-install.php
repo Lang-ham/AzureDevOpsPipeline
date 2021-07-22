edbdc9a7b37067be28e5a47fb0352a75ac02f711
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-07-22 13:28:56 +0000

    b43: fix frequency reported on G-PHY with /new/ firmware

--- wordpress/wp-admin/network/plugin-install.php
+++ wordpress/wp-admin/network/plugin-install.php
@@ -8,4 +8,9 @@
  */
 
 if ( isset( $_GET['tab'] ) && ( 'plugin-information' == $_GET['tab'] ) )
-	define( 'IFRA
+	define( 'IFRAME_REQUEST', true );
+
+/** Load WordPress Administration Bootstrap */
+require_once( dirname( __FILE__ ) . '/admin.php' );
+
+require( ABSPATH . 'wp-admin/plugin-install.php' );
