8a44d9f87574d83882b2b7da4e8e3571ebb06a26
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-08-14 12:58:53 +0000

    Update project-4.md

--- wordpress/wp-content/themes/twentyfifteen/single.php
+++ wordpress/wp-content/themes/twentyfifteen/single.php
@@ -32,4 +32,17 @@ get_header(); ?>
 			the_post_navigation( array(
 				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
 					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="p
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
+
+		// End the loop.
+		endwhile;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
