eb800d1f81708f4fd612069a2aa347c9540ff16b
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-09-03 20:08:37 +0000

    commit test

--- wordpress/wp-content/themes/twentyseventeen/inc/color-patterns.php
+++ wordpress/wp-content/themes/twentyseventeen/inc/color-patterns.php
@@ -342,4 +342,232 @@ body.colors-custom,
 .colors-custom input[type="number"],
 .colors-custom input[type="tel"],
 .colors-custom input[type="range"],
-.colors-custom input[type="
+.colors-custom input[type="date"],
+.colors-custom input[type="month"],
+.colors-custom input[type="week"],
+.colors-custom input[type="time"],
+.colors-custom input[type="datetime"],
+.colors-custom input[type="datetime-local"],
+.colors-custom input[type="color"],
+.colors-custom textarea,
+.colors-custom select,
+.colors-custom fieldset,
+.colors-custom .widget .tagcloud a:hover,
+.colors-custom .widget .tagcloud a:focus,
+.colors-custom .widget.widget_tag_cloud a:hover,
+.colors-custom .widget.widget_tag_cloud a:focus,
+.colors-custom .wp_widget_tag_cloud a:hover,
+.colors-custom .wp_widget_tag_cloud a:focus {
+	border-color: hsl( ' . $hue . ', ' . $saturation . ', 73% ); /* base: #bbb; */
+}
+
+.colors-custom thead th {
+	border-bottom-color: hsl( ' . $hue . ', ' . $saturation . ', 73% ); /* base: #bbb; */
+}
+
+.colors-custom .entry-footer .cat-links .icon,
+.colors-custom .entry-footer .tags-links .icon {
+	color: hsl( ' . $hue . ', ' . $saturation . ', 73% ); /* base: #bbb; */
+}
+
+.colors-custom button.secondary,
+.colors-custom input[type="reset"],
+.colors-custom input[type="button"].secondary,
+.colors-custom input[type="reset"].secondary,
+.colors-custom input[type="submit"].secondary,
+.colors-custom .prev.page-numbers,
+.colors-custom .next.page-numbers {
+	background-color: hsl( ' . $hue . ', ' . $saturation . ', 87% ); /* base: #ddd; */
+}
+
+.colors-custom .widget .tagcloud a,
+.colors-custom .widget.widget_tag_cloud a,
+.colors-custom .wp_widget_tag_cloud a {
+	border-color: hsl( ' . $hue . ', ' . $saturation . ', 87% ); /* base: #ddd; */
+}
+
+.colors-custom.twentyseventeen-front-page article:not(.has-post-thumbnail):not(:first-child),
+.colors-custom .widget ul li {
+	border-top-color: hsl( ' . $hue . ', ' . $saturation . ', 87% ); /* base: #ddd; */
+}
+
+.colors-custom .widget ul li {
+	border-bottom-color: hsl( ' . $hue . ', ' . $saturation . ', 87% ); /* base: #ddd; */
+}
+
+.colors-custom pre,
+.colors-custom mark,
+.colors-custom ins {
+	background: hsl( ' . $hue . ', ' . $saturation . ', 93% ); /* base: #eee; */
+}
+
+.colors-custom .navigation-top,
+.colors-custom .main-navigation > div > ul,
+.colors-custom .pagination,
+.colors-custom .comments-pagination,
+.colors-custom .entry-footer,
+.colors-custom .site-footer {
+	border-top-color: hsl( ' . $hue . ', ' . $saturation . ', 93% ); /* base: #eee; */
+}
+
+.colors-custom .navigation-top,
+.colors-custom .main-navigation li,
+.colors-custom .entry-footer,
+.colors-custom .single-featured-image-header,
+.colors-custom .site-content .wp-playlist-light .wp-playlist-item,
+.colors-custom tr {
+	border-bottom-color: hsl( ' . $hue . ', ' . $saturation . ', 93% ); /* base: #eee; */
+}
+
+.colors-custom .site-content .wp-playlist-light {
+	border-color: hsl( ' . $hue . ', ' . $saturation . ', 93% ); /* base: #eee; */
+}
+
+.colors-custom .site-header,
+.colors-custom .single-featured-image-header {
+	background-color: hsl( ' . $hue . ', ' . $saturation . ', 98% ); /* base: #fafafa; */
+}
+
+.colors-custom button,
+.colors-custom input[type="button"],
+.colors-custom input[type="submit"],
+.colors-custom .entry-footer .edit-link a.post-edit-link,
+.colors-custom .social-navigation a,
+.colors-custom .site-content .wp-playlist-light a.wp-playlist-caption:hover,
+.colors-custom .site-content .wp-playlist-light .wp-playlist-item:hover a,
+.colors-custom .site-content .wp-playlist-light .wp-playlist-item:focus a,
+.colors-custom .site-content .wp-playlist-light .wp-playlist-item:hover,
+.colors-custom .site-content .wp-playlist-light .wp-playlist-item:focus,
+.colors-custom .prev.page-numbers:focus,
+.colors-custom .prev.page-numbers:hover,
+.colors-custom .next.page-numbers:focus,
+.colors-custom .next.page-numbers:hover,
+.colors-custom.has-header-image .site-title,
+.colors-custom.has-header-video .site-title,
+.colors-custom.has-header-image .site-title a,
+.colors-custom.has-header-video .site-title a,
+.colors-custom.has-header-image .site-description,
+.colors-custom.has-header-video .site-description {
+	color: hsl( ' . $hue . ', ' . $saturation . ', 100% ); /* base: #fff; */
+}
+
+body.colors-custom,
+.colors-custom .navigation-top,
+.colors-custom .main-navigation ul {
+	background: hsl( ' . $hue . ', ' . $saturation . ', 100% ); /* base: #fff; */
+}
+
+.colors-custom .widget ul li a,
+.colors-custom .site-footer .widget-area ul li a {
+	-webkit-box-shadow: inset 0 -1px 0 hsl( ' . $hue . ', ' . $saturation . ', 100% ); /* base: rgba(255, 255, 255, 1); */
+	box-shadow: inset 0 -1px 0 hsl( ' . $hue . ', ' . $saturation . ', 100% );  /* base: rgba(255, 255, 255, 1); */
+}
+
+.colors-custom .menu-toggle,
+.colors-custom .menu-toggle:hover,
+.colors-custom .menu-toggle:focus,
+.colors-custom .menu .dropdown-toggle,
+.colors-custom .menu-scroll-down,
+.colors-custom .menu-scroll-down:hover,
+.colors-custom .menu-scroll-down:focus {
+	background-color: transparent;
+}
+
+.colors-custom .widget .tagcloud a,
+.colors-custom .widget .tagcloud a:focus,
+.colors-custom .widget .tagcloud a:hover,
+.colors-custom .widget.widget_tag_cloud a,
+.colors-custom .widget.widget_tag_cloud a:focus,
+.colors-custom .widget.widget_tag_cloud a:hover,
+.colors-custom .wp_widget_tag_cloud a,
+.colors-custom .wp_widget_tag_cloud a:focus,
+.colors-custom .wp_widget_tag_cloud a:hover,
+.colors-custom .entry-footer .edit-link a.post-edit-link:focus,
+.colors-custom .entry-footer .edit-link a.post-edit-link:hover {
+	-webkit-box-shadow: none !important;
+	box-shadow: none !important;
+}
+
+/* Reset non-customizable hover styling for links */
+.colors-custom .entry-content a:hover,
+.colors-custom .entry-content a:focus,
+.colors-custom .entry-summary a:hover,
+.colors-custom .entry-summary a:focus,
+.colors-custom .widget a:hover,
+.colors-custom .widget a:focus,
+.colors-custom .site-footer .widget-area a:hover,
+.colors-custom .site-footer .widget-area a:focus,
+.colors-custom .posts-navigation a:hover,
+.colors-custom .posts-navigation a:focus,
+.colors-custom .widget_authors a:hover strong,
+.colors-custom .widget_authors a:focus strong {
+	-webkit-box-shadow: inset 0 0 0 rgba(0, 0, 0, 0), 0 3px 0 rgba(0, 0, 0, 1);
+	box-shadow: inset 0 0 0 rgba(0, 0, 0, 0), 0 3px 0 rgba(0, 0, 0, 1);
+}
+
+.colors-custom .gallery-item a,
+.colors-custom .gallery-item a:hover,
+.colors-custom .gallery-item a:focus {
+	-webkit-box-shadow: none;
+	box-shadow: none;
+}
+
+@media screen and (min-width: 48em) {
+
+	.colors-custom .nav-links .nav-previous .nav-title .icon,
+	.colors-custom .nav-links .nav-next .nav-title .icon {
+		color: hsl( ' . $hue . ', ' . $saturation . ', 20% ); /* base: #222; */
+	}
+
+	.colors-custom .main-navigation li li:hover,
+	.colors-custom .main-navigation li li.focus {
+		background: hsl( ' . $hue . ', ' . $saturation . ', 46% ); /* base: #767676; */
+	}
+
+	.colors-custom .navigation-top .menu-scroll-down {
+		color: hsl( ' . $hue . ', ' . $saturation . ', 46% ); /* base: #767676; */;
+	}
+
+	.colors-custom abbr[title] {
+		border-bottom-color: hsl( ' . $hue . ', ' . $saturation . ', 46% ); /* base: #767676; */;
+	}
+
+	.colors-custom .main-navigation ul ul {
+		border-color: hsl( ' . $hue . ', ' . $saturation . ', 73% ); /* base: #bbb; */
+		background: hsl( ' . $hue . ', ' . $saturation . ', 100% ); /* base: #fff; */
+	}
+
+	.colors-custom .main-navigation ul li.menu-item-has-children:before,
+	.colors-custom .main-navigation ul li.page_item_has_children:before {
+		border-bottom-color: hsl( ' . $hue . ', ' . $saturation . ', 73% ); /* base: #bbb; */
+	}
+
+	.colors-custom .main-navigation ul li.menu-item-has-children:after,
+	.colors-custom .main-navigation ul li.page_item_has_children:after {
+		border-bottom-color: hsl( ' . $hue . ', ' . $saturation . ', 100% ); /* base: #fff; */
+	}
+
+	.colors-custom .main-navigation li li.focus > a,
+	.colors-custom .main-navigation li li:focus > a,
+	.colors-custom .main-navigation li li:hover > a,
+	.colors-custom .main-navigation li li a:hover,
+	.colors-custom .main-navigation li li a:focus,
+	.colors-custom .main-navigation li li.current_page_item a:hover,
+	.colors-custom .main-navigation li li.current-menu-item a:hover,
+	.colors-custom .main-navigation li li.current_page_item a:focus,
+	.colors-custom .main-navigation li li.current-menu-item a:focus {
+		color: hsl( ' . $hue . ', ' . $saturation . ', 100% ); /* base: #fff; */
+	}
+}';
+
+	/**
+	 * Filters Twenty Seventeen custom colors CSS.
+	 *
+	 * @since Twenty Seventeen 1.0
+	 *
+	 * @param string $css        Base theme colors CSS.
+	 * @param int    $hue        The user's selected color hue.
+	 * @param string $saturation Filtered theme color saturation level.
+	 */
+	return apply_filters( 'twentyseventeen_custom_colors_css', $css, $hue, $saturation );
+}
---
+++ wordpress/wp-content/themes/twentyseventeen/inc/custom-header.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Custom header implementation
+ *
+ * @link https://codex.wordpress.org/Custom_Headers
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ */
+
+/**
+ * Set up the WordPress core custom header feature.
+ *
+ * @uses twentyseventeen_header_style()
+ */
+function twentyseventeen_custom_header_setup() {
+
+	/**
+	 * Filter Twenty Seventeen custom-header support arguments.
+	 *
+	 * @since Twenty Seventeen 1.0
+	 *
+	 * @param array $args {
+	 *     An array of custom-header support arguments.
+	 *
+	 *     @type string $default-image     		Default image of the header.
+	 *     @type string $default_text_color     Default color of the header text.
+	 *     @type int    $width                  Width in pixels of the custom header image. Default 954.
+	 *     @type int    $height                 Height in pixels of the custom header image. Default 1300.
+	 *     @type string $wp-head-callback       Callback function used to styles the header image and text
+	 *                                          displayed on the blog.
+	 *     @type string $flex-height     		Flex support for height of header.
+	 * }
+	 */
+	add_theme_support( 'custom-header', apply_filters( 'twentyseventeen_custom_header_args', array(
+		'default-image'      => get_parent_theme_file_uri( '/assets/images/header.jpg' ),
+		'width'              => 2000,
+		'height'             => 1200,
+		'flex-height'        => true,
+		'video'              => true,
+		'wp-head-callback'   => 'twentyseventeen_header_style',
+	) ) );
+
+	register_default_headers( array(
+		'default-image' => array(
+			'url'           => '%s/assets/images/header.jpg',
+			'thumbnail_url' => '%s/assets/images/header.jpg',
+			'description'   => __( 'Default Header Image', 'twentyseventeen' ),
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'twentyseventeen_custom_header_setup' );
+
+if ( ! function_exists( 'twentyseventeen_header_style' ) ) :
+/**
+ * Styles the header image and text displayed on the blog.
+ *
+ * @see twentyseventeen_custom_header_setup().
+ */
+function twentyseventeen_header_style() {
+	$header_text_color = get_header_textcolor();
+
+	// If no 
