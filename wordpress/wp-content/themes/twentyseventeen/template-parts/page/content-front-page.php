0e7d6a9f5680514a0087d770071d068ade5c7452
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-09-09 18:02:24 +0000

    v0.0.29

--- wordpress/wp-content/themes/twentyseventeen/template-parts/page/content-front-page.php
+++ wordpress/wp-content/themes/twentyseventeen/template-parts/page/content-front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying pages on front page
+ * Displays content for front page
  *
  * @package WordPress
  * @subpackage Twenty_Seventeen
@@ -8,11 +8,8 @@
  * @version 1.0
  */
 
-global $twentyseventeencounter;
-
 ?>
-
-<article id="panel<?php echo $twentyseventeencounter; ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
 
 	<?php if ( has_post_thumbnail() ) :
 		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
@@ -45,33 +42,6 @@ global $twentyseventeencounter;
 					) );
 				?>
 			</div><!-- .entry-content -->
-
-			<?php
-			// Show recent blog posts if is blog posts page (Note that get_option returns a string, so we're casting the result as an int).
-			if ( get_the_ID() === (int) get_option( 'page_for_posts' )  ) : ?>
-
-				<?php // Show four most recent posts.
-				$recent_posts = new WP_Query( array(
-					'posts_per_page'      => 3,
-					'post_status'         => 'publish',
-					'ignore_sticky_posts' => true,
-					'no_found_rows'       => true,
-				) );
-				?>
-
-		 		<?php if ( $recent_posts->have_posts() ) : ?>
-
-					<div class="recent-posts">
-
-						<?php
-						while ( $recent_posts->have_posts() ) : $recent_posts->the_post();
-							get_template_part( 'template-parts/post/content', 'excerpt' );
-						endwhile;
-						wp_reset_postdata();
-						?>
-					</div><!-- .recent-posts -->
-				<?php endif; ?>
-			<?php endif; ?>
 
 		</div><!-- .wrap -->
 	</div><!-- .panel-content -->
