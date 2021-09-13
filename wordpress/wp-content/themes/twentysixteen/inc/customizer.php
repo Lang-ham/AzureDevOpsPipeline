68463e3f90831e0bbb97e012e8bd03565f6f4256
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-09-13 19:16:24 +0000

    Updated Tamil Translations

--- wordpress/wp-content/themes/twentysixteen/inc/customizer.php
+++ wordpress/wp-content/themes/twentysixteen/inc/customizer.php
@@ -2,4 +2,365 @@
 /**
  * Twenty Sixteen Customizer functionality
  *
- * @packag
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+/**
+ * Sets up the WordPress core custom header and custom background features.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @see twentysixteen_header_style()
+ */
+function twentysixteen_custom_header_and_background() {
+	$color_scheme             = twentysixteen_get_color_scheme();
+	$default_background_color = trim( $color_scheme[0], '#' );
+	$default_text_color       = trim( $color_scheme[3], '#' );
+
+	/**
+	 * Filter the arguments used when adding 'custom-background' support in Twenty Sixteen.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 *
+	 * @param array $args {
+	 *     An array of custom-background support arguments.
+	 *
+	 *     @type string $default-color Default color of the background.
+	 * }
+	 */
+	add_theme_support( 'custom-background', apply_filters( 'twentysixteen_custom_background_args', array(
+		'default-color' => $default_background_color,
+	) ) );
+
+	/**
+	 * Filter the arguments used when adding 'custom-header' support in Twenty Sixteen.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 *
+	 * @param array $args {
+	 *     An array of custom-header support arguments.
+	 *
+	 *     @type string $default-text-color Default color of the header text.
+	 *     @type int      $width            Width in pixels of the custom header image. Default 1200.
+	 *     @type int      $height           Height in pixels of the custom header image. Default 280.
+	 *     @type bool     $flex-height      Whether to allow flexible-height header images. Default true.
+	 *     @type callable $wp-head-callback Callback function used to style the header image and text
+	 *                                      displayed on the blog.
+	 * }
+	 */
+	add_theme_support( 'custom-header', apply_filters( 'twentysixteen_custom_header_args', array(
+		'default-text-color'     => $default_text_color,
+		'width'                  => 1200,
+		'height'                 => 280,
+		'flex-height'            => true,
+		'wp-head-callback'       => 'twentysixteen_header_style',
+	) ) );
+}
+add_action( 'after_setup_theme', 'twentysixteen_custom_header_and_background' );
+
+if ( ! function_exists( 'twentysixteen_header_style' ) ) :
+/**
+ * Styles the header text displayed on the site.
+ *
+ * Create your own twentysixteen_header_style() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @see twentysixteen_custom_header_and_background().
+ */
+function twentysixteen_header_style() {
+	// If the header text option is untouched, let's bail.
+	if ( display_header_text() ) {
+		return;
+	}
+
+	// If the header text has been hidden.
+	?>
+	<style type="text/css" id="twentysixteen-header-css">
+		.site-branding {
+			margin: 0 auto 0 0;
+		}
+
+		.site-branding .site-title,
+		.site-description {
+			clip: rect(1px, 1px, 1px, 1px);
+			position: absolute;
+		}
+	</style>
+	<?php
+}
+endif; // twentysixteen_header_style
+
+/**
+ * Adds postMessage support for site title and description for the Customizer.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @param WP_Customize_Manager $wp_customize The Customizer object.
+ */
+function twentysixteen_customize_register( $wp_customize ) {
+	$color_scheme = twentysixteen_get_color_scheme();
+
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector' => '.site-title a',
+			'container_inclusive' => false,
+			'render_callback' => 'twentysixteen_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector' => '.site-description',
+			'container_inclusive' => false,
+			'render_callback' => 'twentysixteen_customize_partial_blogdescription',
+		) );
+	}
+
+	// Add color scheme setting and control.
+	$wp_customize->add_setting( 'color_scheme', array(
+		'default'           => 'default',
+		'sanitize_callback' => 'twentysixteen_sanitize_color_scheme',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( 'color_scheme', array(
+		'label'    => __( 'Base Color Scheme', 'twentysixteen' ),
+		'section'  => 'colors',
+		'type'     => 'select',
+		'choices'  => twentysixteen_get_color_scheme_choices(),
+		'priority' => 1,
+	) );
+
+	// Add page background color setting and control.
+	$wp_customize->add_setting( 'page_background_color', array(
+		'default'           => $color_scheme[1],
+		'sanitize_callback' => 'sanitize_hex_color',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'page_background_color', array(
+		'label'       => __( 'Page Background Color', 'twentysixteen' ),
+		'section'     => 'colors',
+	) ) );
+
+	// Remove the core header textcolor control, as it shares the main text color.
+	$wp_customize->remove_control( 'header_textcolor' );
+
+	// Add link color setting and control.
+	$wp_customize->add_setting( 'link_color', array(
+		'default'           => $color_scheme[2],
+		'sanitize_callback' => 'sanitize_hex_color',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
+		'label'       => __( 'Link Color', 'twentysixteen' ),
+		'section'     => 'colors',
+	) ) );
+
+	// Add main text color setting and control.
+	$wp_customize->add_setting( 'main_text_color', array(
+		'default'           => $color_scheme[3],
+		'sanitize_callback' => 'sanitize_hex_color',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'main_text_color', array(
+		'label'       => __( 'Main Text Color', 'twentysixteen' ),
+		'section'     => 'colors',
+	) ) );
+
+	// Add secondary text color setting and control.
+	$wp_customize->add_setting( 'secondary_text_color', array(
+		'default'           => $color_scheme[4],
+		'sanitize_callback' => 'sanitize_hex_color',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'secondary_text_color', array(
+		'label'       => __( 'Secondary Text Color', 'twentysixteen' ),
+		'section'     => 'colors',
+	) ) );
+}
+add_action( 'customize_register', 'twentysixteen_customize_register', 11 );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @since Twenty Sixteen 1.2
+ * @see twentysixteen_customize_register()
+ *
+ * @return void
+ */
+function twentysixteen_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @since Twenty Sixteen 1.2
+ * @see twentysixteen_customize_register()
+ *
+ * @return void
+ */
+function twentysixteen_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Registers color schemes for Twenty Sixteen.
+ *
+ * Can be filtered with {@see 'twentysixteen_color_schemes'}.
+ *
+ * The order of colors in a colors array:
+ * 1. Main Background Color.
+ * 2. Page Background Color.
+ * 3. Link Color.
+ * 4. Main Text Color.
+ * 5. Secondary Text Color.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @return array An associative array of color scheme options.
+ */
+function twentysixteen_get_color_schemes() {
+	/**
+	 * Filter the color schemes registered for use with Twenty Sixteen.
+	 *
+	 * The default schemes include 'default', 'dark', 'gray', 'red', and 'yellow'.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 *
+	 * @param array $schemes {
+	 *     Associative array of color schemes data.
+	 *
+	 *     @type array $slug {
+	 *         Associative array of information for setting up the color scheme.
+	 *
+	 *         @type string $label  Color scheme label.
+	 *         @type array  $colors HEX codes for default colors prepended with a hash symbol ('#').
+	 *                              Colors are defined in the following order: Main background, page
+	 *                              background, link, main text, secondary text.
+	 *     }
+	 * }
+	 */
+	return apply_filters( 'twentysixteen_color_schemes', array(
+		'default' => array(
+			'label'  => __( 'Default', 'twentysixteen' ),
+			'colors' => array(
+				'#1a1a1a',
+				'#ffffff',
+				'#007acc',
+				'#1a1a1a',
+				'#686868',
+			),
+		),
+		'dark' => array(
+			'label'  => __( 'Dark', 'twentysixteen' ),
+			'colors' => array(
+				'#262626',
+				'#1a1a1a',
+				'#9adffd',
+				'#e5e5e5',
+				'#c1c1c1',
+			),
+		),
+		'gray' => array(
+			'label'  => __( 'Gray', 'twentysixteen' ),
+			'colors' => array(
+				'#616a73',
+				'#4d545c',
+				'#c7c7c7',
+				'#f2f2f2',
+				'#f2f2f2',
+			),
+		),
+		'red' => array(
+			'label'  => __( 'Red', 'twentysixteen' ),
+			'colors' => array(
+				'#ffffff',
+				'#ff675f',
+				'#640c1f',
+				'#402b30',
+				'#402b30',
+			),
+		),
+		'yellow' => array(
+			'label'  => __( 'Yellow', 'twentysixteen' ),
+			'colors' => array(
+				'#3b3721',
+				'#ffef8e',
+				'#774e24',
+				'#3b3721',
+				'#5b4d3e',
+			),
+		),
+	) );
+}
+
+if ( ! function_exists( 'twentysixteen_get_color_scheme' ) ) :
+/**
+ * Retrieves the current Twenty Sixteen color scheme.
+ *
+ * Create your own twentysixteen_get_color_scheme() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @return array An associative array of either the current or default color scheme HEX values.
+ */
+function twentysixteen_get_color_scheme() {
+	$color_scheme_option = get_theme_mod( 'color_scheme', 'default' );
+	$color_schemes       = twentysixteen_get_color_schemes();
+
+	if ( array_key_exists( $color_scheme_option, $color_schemes ) ) {
+		return $color_schemes[ $color_scheme_option ]['colors'];
+	}
+
+	return $color_schemes['default']['colors'];
+}
+endif; // twentysixteen_get_color_scheme
+
+if ( ! function_exists( 'twentysixteen_get_color_scheme_choices' ) ) :
+/**
+ * Retrieves an array of color scheme choices registered for Twenty Sixteen.
+ *
+ * Create your own twentysixteen_get_color_scheme_choices() function to override
+ * in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @return array Array of color schemes.
+ */
+function twentysixteen_get_color_scheme_choices() {
+	$color_schemes                = twentysixteen_get_color_schemes();
+	$color_scheme_control_options = array();
+
+	foreach ( $color_schemes as $color_scheme => $value ) {
+		$color_scheme_control_options[ $color_scheme ] = $value['label'];
+	}
+
+	return $color_scheme_control_options;
+}
+endif; // twentysixteen_get_color_scheme_choices
+
+
+if ( ! function_exists( 'twentysixteen_sanitize_color_scheme' ) ) :
+/**
+ * Handles sanitization for Twenty Sixteen color schemes.
+ *
+ * Create your own twentysixteen_sanitize_color_scheme() function to override
+ * in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @param string $value Color scheme name value.
+ * @return string Color scheme name.
+ */
+function twentysixteen_sanitize_color_scheme( $value ) {
+	$color_schemes = twentysixteen_get_color_scheme_choices();
+
+	if ( ! array_key_exists( $value
