e7b744ee1aaf7bd7b69ee206684ff4cc9b9e2e69
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-11-07 10:21:18 +0000

    indent fix

---
+++ wordpress/wp-includes/Requests/Exception/HTTP/511.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Exception for 511 Network Authentication Required responses
+ *
+ * @see https://tools.ietf.org/html/rfc6585
+ * @package Requests
+ */
+
+/**
+ * Exception for 511 Network Authentication Required responses
+ *
+ * @see https://tool
