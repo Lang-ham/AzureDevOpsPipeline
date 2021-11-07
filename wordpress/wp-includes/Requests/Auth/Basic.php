48c72ee798daaa6e724eb1137cd71321ac0286a8
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-11-07 06:34:58 +0000

    iwlwifi: always copy first 16 bytes of commands

---
+++ wordpress/wp-includes/Requests/Auth/Basic.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Basic Authentication provider
+ *
+ * @package Requests
+ * @subpackage Authentication
+ */
+
+/**
+ * Basic Authentication provider
+ *
+ * Provides a handler for Basic HTTP authentication via the Authorization
+ * header.
+ *
+ * @package Requests
+ * @subpackage Authentication
+ */
+class Requests_Auth_Basic implements Requests_Auth {
+	/**
+	 * Username
+	 *
+	 * @var string
+	 */
+	public $user;
+
+	/**
+	 * Password
+	 *
+	 * @var string
+	 */
+	public
