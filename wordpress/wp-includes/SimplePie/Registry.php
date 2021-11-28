a96b3d91058d57a2df94ce456d3b2c086475455d
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-11-28 13:05:40 +0000

    Various small changes.

---
+++ wordpress/wp-includes/SimplePie/Registry.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * SimplePie
+ *
+ * A PHP-Based RSS and Atom Feed Framework.
+ * Takes the hard work out of managing a complete RSS/Atom solution.
+ *
+ * Copyright (c) 2004-2012, Ryan Parman, Geoffrey Sneddon, Ryan McCue, and contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 	* Redistributions of source code must retain the above copyri
