00e2708d28496b6b7861c8f4bde3115863bd7c59
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2021-12-08 08:07:04 +0000

    win fix

--- wordpress/wp-includes/category-template.php
+++ wordpress/wp-includes/category-template.php
@@ -180,4 +180,271 @@ function get_the_category_list( $separator = '', $parents = '', $post_id = false
 				$thelist .= $separator;
 			switch ( strtolower( $parents ) ) {
 				case 'multiple':
-					if ( $category-
+					if ( $category->parent )
+						$thelist .= get_category_parents( $category->parent, true, $separator );
+					$thelist .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" ' . $rel . '>' . $category->name.'</a>';
+					break;
+				case 'single':
+					$thelist .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" ' . $rel . '>';
+					if ( $category->parent )
+						$thelist .= get_category_parents( $category->parent, false, $separator );
+					$thelist .= "$category->name</a>";
+					break;
+				case '':
+				default:
+					$thelist .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" ' . $rel . '>' . $category->name.'</a>';
+			}
+			++$i;
+		}
+	}
+
+	/**
+	 * Filters the category or list of categories.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param string $thelist   List of categories for the current post.
+	 * @param string $separator Separator used between the categories.
+	 * @param string $parents   How to display the category parents. Accepts 'multiple',
+	 *                          'single', or empty.
+	 */
+	return apply_filters( 'the_category', $thelist, $separator, $parents );
+}
+
+/**
+ * Check if the current post is within any of the given categories.
+ *
+ * The given categories are checked against the post's categories' term_ids, names and slugs.
+ * Categories given as integers will only be checked against the post's categories' term_ids.
+ *
+ * Prior to v2.5 of WordPress, category names were not supported.
+ * Prior to v2.7, category slugs were not supported.
+ * Prior to v2.7, only one category could be compared: in_category( $single_category ).
+ * Prior to v2.7, this function could only be used in the WordPress Loop.
+ * As of 2.7, the function can be used anywhere if it is provided a post ID or post object.
+ *
+ * @since 1.2.0
+ *
+ * @param int|string|array $category Category ID, name or slug, or array of said.
+ * @param int|object $post Optional. Post to check instead of the current post. (since 2.7.0)
+ * @return bool True if the current post is in any of the given categories.
+ */
+function in_category( $category, $post = null ) {
+	if ( empty( $category ) )
+		return false;
+
+	return has_category( $category, $post );
+}
+
+/**
+ * Display category list for a post in either HTML list or custom format.
+ *
+ * @since 0.71
+ *
+ * @param string $separator Optional. Separator between the categories. By default, the links are placed
+ *                          in an unordered list. An empty string will result in the default behavior.
+ * @param string $parents Optional. How to display the parents.
+ * @param int $post_id Optional. Post ID to retrieve categories.
+ */
+function the_category( $separator = '', $parents = '', $post_id = false ) {
+	echo get_the_category_list( $separator, $parents, $post_id );
+}
+
+/**
+ * Retrieve category description.
+ *
+ * @since 1.0.0
+ *
+ * @param int $category Optional. Category ID. Will use global category ID by default.
+ * @return string Category description, available.
+ */
+function category_description( $category = 0 ) {
+	return term_description( $category, 'category' );
+}
+
+/**
+ * Display or retrieve the HTML dropdown list of categories.
+ *
+ * The 'hierarchical' argument, which is disabled by default, will override the
+ * depth argument, unless it is true. When the argument is false, it will
+ * display all of the categories. When it is enabled it will use the value in
+ * the 'depth' argument.
+ *
+ * @since 2.1.0
+ * @since 4.2.0 Introduced the `value_field` argument.
+ * @since 4.6.0 Introduced the `required` argument.
+ *
+ * @param string|array $args {
+ *     Optional. Array or string of arguments to generate a categories drop-down element. See WP_Term_Query::__construct()
+ *     for information on additional accepted arguments.
+ *
+ *     @type string       $show_option_all   Text to display for showing all categories. Default empty.
+ *     @type string       $show_option_none  Text to display for showing no categories. Default empty.
+ *     @type string       $option_none_value Value to use when no category is selected. Default empty.
+ *     @type string       $orderby           Which column to use for ordering categories. See get_terms() for a list
+ *                                           of accepted values. Default 'id' (term_id).
+ *     @type bool         $pad_counts        See get_terms() for an argument description. Default false.
+ *     @type bool|int     $show_count        Whether to include post counts. Accepts 0, 1, or their bool equivalents.
+ *                                           Default 0.
+ *     @type bool|int     $echo              Whether to echo or return the generated markup. Accepts 0, 1, or their
+ *                                           bool equivalents. Default 1.
+ *     @type bool|int     $hierarchical      Whether to traverse the taxonomy hierarchy. Accepts 0, 1, or their bool
+ *                                           equivalents. Default 0.
+ *     @type int          $depth             Maximum depth. Default 0.
+ *     @type int          $tab_index         Tab index for the select element. Default 0 (no tabindex).
+ *     @type string       $name              Value for the 'name' attribute of the select element. Default 'cat'.
+ *     @type string       $id                Value for the 'id' attribute of the select element. Defaults to the value
+ *                                           of `$name`.
+ *     @type string       $class             Value for the 'class' attribute of the select element. Default 'postform'.
+ *     @type int|string   $selected          Value of the option that should be selected. Default 0.
+ *     @type string       $value_field       Term field that should be used to populate the 'value' attribute
+ *                                           of the option elements. Accepts any valid term field: 'term_id', 'name',
+ *                                           'slug', 'term_group', 'term_taxonomy_id', 'taxonomy', 'description',
+ *                                           'parent', 'count'. Default 'term_id'.
+ *     @type string|array $taxonomy          Name of the category or categories to retrieve. Default 'category'.
+ *     @type bool         $hide_if_empty     True to skip generating markup if no categories are found.
+ *                                           Default false (create select element even if no categories are found).
+ *     @type bool         $required          Whether the `<select>` element should have the HTML5 'required' attribute.
+ *                                           Default false.
+ * }
+ * @return string HTML content only if 'echo' argument is 0.
+ */
+function wp_dropdown_categories( $args = '' ) {
+	$defaults = array(
+		'show_option_all'   => '',
+		'show_option_none'  => '',
+		'orderby'           => 'id',
+		'order'             => 'ASC',
+		'show_count'        => 0,
+		'hide_empty'        => 1,
+		'child_of'          => 0,
+		'exclude'           => '',
+		'echo'              => 1,
+		'selected'          => 0,
+		'hierarchical'      => 0,
+		'name'              => 'cat',
+		'id'                => '',
+		'class'             => 'postform',
+		'depth'             => 0,
+		'tab_index'         => 0,
+		'taxonomy'          => 'category',
+		'hide_if_empty'     => false,
+		'option_none_value' => -1,
+		'value_field'       => 'term_id',
+		'required'          => false,
+	);
+
+	$defaults['selected'] = ( is_category() ) ? get_query_var( 'cat' ) : 0;
+
+	// Back compat.
+	if ( isset( $args['type'] ) && 'link' == $args['type'] ) {
+		_deprecated_argument( __FUNCTION__, '3.0.0',
+			/* translators: 1: "type => link", 2: "taxonomy => link_category" */
+			sprintf( __( '%1$s is deprecated. Use %2$s instead.' ),
+				'<code>type => link</code>',
+				'<code>taxonomy => link_category</code>'
+			)
+		);
+		$args['taxonomy'] = 'link_category';
+	}
+
+	$r = wp_parse_args( $args, $defaults );
+	$option_none_value = $r['option_none_value'];
+
+	if ( ! isset( $r['pad_counts'] ) && $r['show_count'] && $r['hierarchical'] ) {
+		$r['pad_counts'] = true;
+	}
+
+	$tab_index = $r['tab_index'];
+
+	$tab_index_attribute = '';
+	if ( (int) $tab_index > 0 ) {
+		$tab_index_attribute = " tabindex=\"$tab_index\"";
+	}
+
+	// Avoid clashes with the 'name' param of get_terms().
+	$get_terms_args = $r;
+	unset( $get_terms_args['name'] );
+	$categories = get_terms( $r['taxonomy'], $get_terms_args );
+
+	$name = esc_attr( $r['name'] );
+	$class = esc_attr( $r['class'] );
+	$id = $r['id'] ? esc_attr( $r['id'] ) : $name;
+	$required = $r['required'] ? 'required' : '';
+
+	if ( ! $r['hide_if_empty'] || ! empty( $categories ) ) {
+		$output = "<select $required name='$name' id='$id' class='$class' $tab_index_attribute>\n";
+	} else {
+		$output = '';
+	}
+	if ( empty( $categories ) && ! $r['hide_if_empty'] && ! empty( $r['show_option_none'] ) ) {
+
+		/**
+		 * Filters a taxonomy drop-down display element.
+		 *
+		 * A variety of taxonomy drop-down display elements can be modified
+		 * just prior to display via this filter. Filterable arguments include
+		 * 'show_option_none', 'show_option_all', and various forms of the
+		 * term name.
+		 *
+		 * @since 1.2.0
+		 *
+		 * @see wp_dropdown_categories()
+		 *
+		 * @param string       $element  Category name.
+		 * @param WP_Term|null $category The category object, or null if there's no corresponding category.
+		 */
+		$show_option_none = apply_filters( 'list_cats', $r['show_option_none'], null );
+		$output .= "\t<option value='" . esc_attr( $option_none_value ) . "' selected='selected'>$show_option_none</option>\n";
+	}
+
+	if ( ! empty( $categories ) ) {
+
+		if ( $r['show_option_all'] ) {
+
+			/** This filter is documented in wp-includes/category-template.php */
+			$show_option_all = apply_filters( 'list_cats', $r['show_option_all'], null );
+			$selected = ( '0' === strval($r['selected']) ) ? " selected='selected'" : '';
+			$output .= "\t<option value='0'$selected>$show_option_all</option>\n";
+		}
+
+		if ( $r['show_option_none'] ) {
+
+			/** This filter is documented in wp-includes/category-template.php */
+			$show_option_none = apply_filters( 'list_cats', $r['show_option_none'], null );
+			$selected = selected( $option_none_value, $r['selected'], false );
+			$output .= "\t<option value='" . esc_attr( $option_none_value ) . "'$selected>$show_option_none</option>\n";
+		}
+
+		if ( $r['hierarchical'] ) {
+			$depth = $r['depth'];  // Walk the full depth.
+		} else {
+			$depth = -1; // Flat.
+		}
+		$output .= walk_category_dropdown_tree( $categories, $depth, $r );
+	}
+
+	if ( ! $r['hide_if_empty'] || ! empty( $categories ) ) {
+		$output .= "</select>\n";
+	}
+	/**
+	 * Filters the taxonomy drop-down output.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @param string $output HTML output.
+	 * @param array  $r      Arguments used to build the drop-down.
+	 */
+	$output = apply_filters( 'wp_dropdown_cats', $output, $r );
+
+	if ( $r['echo'] ) {
+		echo $output;
+	}
+	return $output;
+}
+
+/**
+ * Display or retrieve the HTML list of categories.
+ *
+ * @since 2.1.0
+ * @since 4.4.0 Introduced
