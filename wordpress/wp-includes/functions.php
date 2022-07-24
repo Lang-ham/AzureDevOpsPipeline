16dc28639b171493ac2e54947072fb1631963316
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2022-07-24 14:38:16 +0000

    Release v0.0.3

--- wordpress/wp-includes/functions.php
+++ wordpress/wp-includes/functions.php
@@ -245,4 +245,381 @@ function number_format_i18n( $number, $decimals = 0 ) {
  *
  * Please note that integers in PHP are limited to 32 bits, unless they are on
  * 64 bit architecture, then they have 64 bit size. If you need to place the
- * larger
+ * larger size then what PHP integer type will hold, then use a string. It will
+ * be converted to a double, which should always have 64 bit length.
+ *
+ * Technically the correct unit names for powers of 1024 are KiB, MiB etc.
+ *
+ * @since 2.3.0
+ *
+ * @param int|string $bytes    Number of bytes. Note max integer size for integers.
+ * @param int        $decimals Optional. Precision of number of decimal places. Default 0.
+ * @return string|false False on failure. Number string on success.
+ */
+function size_format( $bytes, $decimals = 0 ) {
+	$quant = array(
+		'TB' => TB_IN_BYTES,
+		'GB' => GB_IN_BYTES,
+		'MB' => MB_IN_BYTES,
+		'KB' => KB_IN_BYTES,
+		'B'  => 1,
+	);
+
+	if ( 0 === $bytes ) {
+		return number_format_i18n( 0, $decimals ) . ' B';
+	}
+
+	foreach ( $quant as $unit => $mag ) {
+		if ( doubleval( $bytes ) >= $mag ) {
+			return number_format_i18n( $bytes / $mag, $decimals ) . ' ' . $unit;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Get the week start and end from the datetime or date string from MySQL.
+ *
+ * @since 0.71
+ *
+ * @param string     $mysqlstring   Date or datetime field type from MySQL.
+ * @param int|string $start_of_week Optional. Start of the week as an integer. Default empty string.
+ * @return array Keys are 'start' and 'end'.
+ */
+function get_weekstartend( $mysqlstring, $start_of_week = '' ) {
+	// MySQL string year.
+	$my = substr( $mysqlstring, 0, 4 );
+
+	// MySQL string month.
+	$mm = substr( $mysqlstring, 8, 2 );
+
+	// MySQL string day.
+	$md = substr( $mysqlstring, 5, 2 );
+
+	// The timestamp for MySQL string day.
+	$day = mktime( 0, 0, 0, $md, $mm, $my );
+
+	// The day of the week from the timestamp.
+	$weekday = date( 'w', $day );
+
+	if ( !is_numeric($start_of_week) )
+		$start_of_week = get_option( 'start_of_week' );
+
+	if ( $weekday < $start_of_week )
+		$weekday += 7;
+
+	// The most recent week start day on or before $day.
+	$start = $day - DAY_IN_SECONDS * ( $weekday - $start_of_week );
+
+	// $start + 1 week - 1 second.
+	$end = $start + WEEK_IN_SECONDS - 1;
+	return compact( 'start', 'end' );
+}
+
+/**
+ * Unserialize value only if it was serialized.
+ *
+ * @since 2.0.0
+ *
+ * @param string $original Maybe unserialized original, if is needed.
+ * @return mixed Unserialized data can be any type.
+ */
+function maybe_unserialize( $original ) {
+	if ( is_serialized( $original ) ) // don't attempt to unserialize data that wasn't serialized going in
+		return @unserialize( $original );
+	return $original;
+}
+
+/**
+ * Check value to find if it was serialized.
+ *
+ * If $data is not an string, then returned value will always be false.
+ * Serialized data is always a string.
+ *
+ * @since 2.0.5
+ *
+ * @param string $data   Value to check to see if was serialized.
+ * @param bool   $strict Optional. Whether to be strict about the end of the string. Default true.
+ * @return bool False if not serialized and true if it was.
+ */
+function is_serialized( $data, $strict = true ) {
+	// if it isn't a string, it isn't serialized.
+	if ( ! is_string( $data ) ) {
+		return false;
+	}
+	$data = trim( $data );
+ 	if ( 'N;' == $data ) {
+		return true;
+	}
+	if ( strlen( $data ) < 4 ) {
+		return false;
+	}
+	if ( ':' !== $data[1] ) {
+		return false;
+	}
+	if ( $strict ) {
+		$lastc = substr( $data, -1 );
+		if ( ';' !== $lastc && '}' !== $lastc ) {
+			return false;
+		}
+	} else {
+		$semicolon = strpos( $data, ';' );
+		$brace     = strpos( $data, '}' );
+		// Either ; or } must exist.
+		if ( false === $semicolon && false === $brace )
+			return false;
+		// But neither must be in the first X characters.
+		if ( false !== $semicolon && $semicolon < 3 )
+			return false;
+		if ( false !== $brace && $brace < 4 )
+			return false;
+	}
+	$token = $data[0];
+	switch ( $token ) {
+		case 's' :
+			if ( $strict ) {
+				if ( '"' !== substr( $data, -2, 1 ) ) {
+					return false;
+				}
+			} elseif ( false === strpos( $data, '"' ) ) {
+				return false;
+			}
+			// or else fall through
+		case 'a' :
+		case 'O' :
+			return (bool) preg_match( "/^{$token}:[0-9]+:/s", $data );
+		case 'b' :
+		case 'i' :
+		case 'd' :
+			$end = $strict ? '$' : '';
+			return (bool) preg_match( "/^{$token}:[0-9.E-]+;$end/", $data );
+	}
+	return false;
+}
+
+/**
+ * Check whether serialized data is of string type.
+ *
+ * @since 2.0.5
+ *
+ * @param string $data Serialized data.
+ * @return bool False if not a serialized string, true if it is.
+ */
+function is_serialized_string( $data ) {
+	// if it isn't a string, it isn't a serialized string.
+	if ( ! is_string( $data ) ) {
+		return false;
+	}
+	$data = trim( $data );
+	if ( strlen( $data ) < 4 ) {
+		return false;
+	} elseif ( ':' !== $data[1] ) {
+		return false;
+	} elseif ( ';' !== substr( $data, -1 ) ) {
+		return false;
+	} elseif ( $data[0] !== 's' ) {
+		return false;
+	} elseif ( '"' !== substr( $data, -2, 1 ) ) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+/**
+ * Serialize data, if needed.
+ *
+ * @since 2.0.5
+ *
+ * @param string|array|object $data Data that might be serialized.
+ * @return mixed A scalar data
+ */
+function maybe_serialize( $data ) {
+	if ( is_array( $data ) || is_object( $data ) )
+		return serialize( $data );
+
+	// Double serialization is required for backward compatibility.
+	// See https://core.trac.wordpress.org/ticket/12930
+	// Also the world will end. See WP 3.6.1.
+	if ( is_serialized( $data, false ) )
+		return serialize( $data );
+
+	return $data;
+}
+
+/**
+ * Retrieve post title from XMLRPC XML.
+ *
+ * If the title element is not part of the XML, then the default post title from
+ * the $post_default_title will be used instead.
+ *
+ * @since 0.71
+ *
+ * @global string $post_default_title Default XML-RPC post title.
+ *
+ * @param string $content XMLRPC XML Request content
+ * @return string Post title
+ */
+function xmlrpc_getposttitle( $content ) {
+	global $post_default_title;
+	if ( preg_match( '/<title>(.+?)<\/title>/is', $content, $matchtitle ) ) {
+		$post_title = $matchtitle[1];
+	} else {
+		$post_title = $post_default_title;
+	}
+	return $post_title;
+}
+
+/**
+ * Retrieve the post category or categories from XMLRPC XML.
+ *
+ * If the category element is not found, then the default post category will be
+ * used. The return type then would be what $post_default_category. If the
+ * category is found, then it will always be an array.
+ *
+ * @since 0.71
+ *
+ * @global string $post_default_category Default XML-RPC post category.
+ *
+ * @param string $content XMLRPC XML Request content
+ * @return string|array List of categories or category name.
+ */
+function xmlrpc_getpostcategory( $content ) {
+	global $post_default_category;
+	if ( preg_match( '/<category>(.+?)<\/category>/is', $content, $matchcat ) ) {
+		$post_category = trim( $matchcat[1], ',' );
+		$post_category = explode( ',', $post_category );
+	} else {
+		$post_category = $post_default_category;
+	}
+	return $post_category;
+}
+
+/**
+ * XMLRPC XML content without title and category elements.
+ *
+ * @since 0.71
+ *
+ * @param string $content XML-RPC XML Request content.
+ * @return string XMLRPC XML Request content without title and category elements.
+ */
+function xmlrpc_removepostdata( $content ) {
+	$content = preg_replace( '/<title>(.+?)<\/title>/si', '', $content );
+	$content = preg_replace( '/<category>(.+?)<\/category>/si', '', $content );
+	$content = trim( $content );
+	return $content;
+}
+
+/**
+ * Use RegEx to extract URLs from arbitrary content.
+ *
+ * @since 3.7.0
+ *
+ * @param string $content Content to extract URLs from.
+ * @return array URLs found in passed string.
+ */
+function wp_extract_urls( $content ) {
+	preg_match_all(
+		"#([\"']?)("
+			. "(?:([\w-]+:)?//?)"
+			. "[^\s()<>]+"
+			. "[.]"
+			. "(?:"
+				. "\([\w\d]+\)|"
+				. "(?:"
+					. "[^`!()\[\]{};:'\".,<>«»“”‘’\s]|"
+					. "(?:[:]\d+)?/?"
+				. ")+"
+			. ")"
+		. ")\\1#",
+		$content,
+		$post_links
+	);
+
+	$post_links = array_unique( array_map( 'html_entity_decode', $post_links[2] ) );
+
+	return array_values( $post_links );
+}
+
+/**
+ * Check content for video and audio links to add as enclosures.
+ *
+ * Will not add enclosures that have already been added and will
+ * remove enclosures that are no longer in the post. This is called as
+ * pingbacks and trackbacks.
+ *
+ * @since 1.5.0
+ *
+ * @global wpdb $wpdb WordPress database abstraction object.
+ *
+ * @param string $content Post Content.
+ * @param int    $post_ID Post ID.
+ */
+function do_enclose( $content, $post_ID ) {
+	global $wpdb;
+
+	//TODO: Tidy this ghetto code up and make the debug code optional
+	include_once( ABSPATH . WPINC . '/class-IXR.php' );
+
+	$post_links = array();
+
+	$pung = get_enclosed( $post_ID );
+
+	$post_links_temp = wp_extract_urls( $content );
+
+	foreach ( $pung as $link_test ) {
+		if ( ! in_array( $link_test, $post_links_temp ) ) { // link no longer in post
+			$mids = $wpdb->get_col( $wpdb->prepare("SELECT meta_id FROM $wpdb->postmeta WHERE post_id = %d AND meta_key = 'enclosure' AND meta_value LIKE %s", $post_ID, $wpdb->esc_like( $link_test ) . '%') );
+			foreach ( $mids as $mid )
+				delete_metadata_by_mid( 'post', $mid );
+		}
+	}
+
+	foreach ( (array) $post_links_temp as $link_test ) {
+		if ( !in_array( $link_test, $pung ) ) { // If we haven't pung it already
+			$test = @parse_url( $link_test );
+			if ( false === $test )
+				continue;
+			if ( isset( $test['query'] ) )
+				$post_links[] = $link_test;
+			elseif ( isset($test['path']) && ( $test['path'] != '/' ) &&  ($test['path'] != '' ) )
+				$post_links[] = $link_test;
+		}
+	}
+
+	/**
+	 * Filters the list of enclosure links before querying the database.
+	 *
+	 * Allows for the addition and/or removal of potential enclosures to save
+	 * to postmeta before checking the database for existing enclosures.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param array $post_links An array of enclosure links.
+	 * @param int   $post_ID    Post ID.
+	 */
+	$post_links = apply_filters( 'enclosure_links', $post_links, $post_ID );
+
+	foreach ( (array) $post_links as $url ) {
+		if ( $url != '' && !$wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE post_id = %d AND meta_key = 'enclosure' AND meta_value LIKE %s", $post_ID, $wpdb->esc_like( $url ) . '%' ) ) ) {
+
+			if ( $headers = wp_get_http_headers( $url) ) {
+				$len = isset( $headers['content-length'] ) ? (int) $headers['content-length'] : 0;
+				$type = isset( $headers['content-type'] ) ? $headers['content-type'] : '';
+				$allowed_types = array( 'video', 'audio' );
+
+				// Check to see if we can figure out the mime type from
+				// the extension
+				$url_parts = @parse_url( $url );
+				if ( false !== $url_parts ) {
+					$extension = pathinfo( $url_parts['path'], PATHINFO_EXTENSION );
+					if ( !empty( $extension ) ) {
+						foreach ( wp_get_mime_types() as $exts => $mime ) {
+							if ( preg_match( '!^(' . $exts . ')$!i', $extension ) ) {
+								$type = $mime;
+								break;
+							}
+						}
+					}
+		
