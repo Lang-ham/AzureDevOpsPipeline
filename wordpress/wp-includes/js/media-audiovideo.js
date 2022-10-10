df2b8e90254a99d71056f1d8782c1ea8892eee3c
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2022-10-10 14:44:57 +0000

    clean up warnings

--- wordpress/wp-includes/js/media-audiovideo.js
+++ wordpress/wp-includes/js/media-audiovideo.js
@@ -869,4 +869,237 @@ MediaDetails = AttachmentDisplay.extend(/** @lends wp.media.view.MediaDetails.pr
 	},
 
 	events: function(){
-		return
+		return _.extend( {
+			'click .remove-setting' : 'removeSetting',
+			'change .content-track' : 'setTracks',
+			'click .remove-track' : 'setTracks',
+			'click .add-media-source' : 'addSource'
+		}, AttachmentDisplay.prototype.events );
+	},
+
+	prepare: function() {
+		return _.defaults({
+			model: this.model.toJSON()
+		}, this.options );
+	},
+
+	/**
+	 * Remove a setting's UI when the model unsets it
+	 *
+	 * @fires wp.media.view.MediaDetails#media:setting:remove
+	 *
+	 * @param {Event} e
+	 */
+	removeSetting : function(e) {
+		var wrap = $( e.currentTarget ).parent(), setting;
+		setting = wrap.find( 'input' ).data( 'setting' );
+
+		if ( setting ) {
+			this.model.unset( setting );
+			this.trigger( 'media:setting:remove', this );
+		}
+
+		wrap.remove();
+	},
+
+	/**
+	 *
+	 * @fires wp.media.view.MediaDetails#media:setting:remove
+	 */
+	setTracks : function() {
+		var tracks = '';
+
+		_.each( this.$('.content-track'), function(track) {
+			tracks += $( track ).val();
+		} );
+
+		this.model.set( 'content', tracks );
+		this.trigger( 'media:setting:remove', this );
+	},
+
+	addSource : function( e ) {
+		this.controller.lastMime = $( e.currentTarget ).data( 'mime' );
+		this.controller.setState( 'add-' + this.controller.defaults.id + '-source' );
+	},
+
+	loadPlayer: function () {
+		this.players.push( new MediaElementPlayer( this.media, this.settings ) );
+		this.scriptXhr = false;
+	},
+
+	setPlayer : function() {
+		var src;
+
+		if ( this.players.length || ! this.media || this.scriptXhr ) {
+			return;
+		}
+
+		src = this.model.get( 'src' );
+
+		if ( src && src.indexOf( 'vimeo' ) > -1 && ! ( 'Vimeo' in window ) ) {
+			this.scriptXhr = $.getScript( 'https://player.vimeo.com/api/player.js', _.bind( this.loadPlayer, this ) );
+		} else {
+			this.loadPlayer();
+		}
+	},
+
+	/**
+	 * @abstract
+	 */
+	setMedia : function() {
+		return this;
+	},
+
+	success : function(mejs) {
+		var autoplay = mejs.attributes.autoplay && 'false' !== mejs.attributes.autoplay;
+
+		if ( 'flash' === mejs.pluginType && autoplay ) {
+			mejs.addEventListener( 'canplay', function() {
+				mejs.play();
+			}, false );
+		}
+
+		this.mejs = mejs;
+	},
+
+	/**
+	 * @returns {media.view.MediaDetails} Returns itself to allow chaining
+	 */
+	render: function() {
+		AttachmentDisplay.prototype.render.apply( this, arguments );
+
+		setTimeout( _.bind( function() {
+			this.resetFocus();
+		}, this ), 10 );
+
+		this.settings = _.defaults( {
+			success : this.success
+		}, wp.media.mixin.mejsSettings );
+
+		return this.setMedia();
+	},
+
+	resetFocus: function() {
+		this.$( '.embed-media-settings' ).scrollTop( 0 );
+	}
+},/** @lends wp.media.view.MediaDetails */{
+	instances : 0,
+	/**
+	 * When multiple players in the DOM contain the same src, things get weird.
+	 *
+	 * @param {HTMLElement} elem
+	 * @returns {HTMLElement}
+	 */
+	prepareSrc : function( elem ) {
+		var i = MediaDetails.instances++;
+		_.each( $( elem ).find( 'source' ), function( source ) {
+			source.src = [
+				source.src,
+				source.src.indexOf('?') > -1 ? '&' : '?',
+				'_=',
+				i
+			].join('');
+		} );
+
+		return elem;
+	}
+});
+
+module.exports = MediaDetails;
+
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+var MediaDetails = wp.media.view.MediaDetails,
+	AudioDetails;
+
+/**
+ * wp.media.view.AudioDetails
+ *
+ * @memberOf wp.media.view
+ *
+ * @class
+ * @augments wp.media.view.MediaDetails
+ * @augments wp.media.view.Settings.AttachmentDisplay
+ * @augments wp.media.view.Settings
+ * @augments wp.media.View
+ * @augments wp.Backbone.View
+ * @augments Backbone.View
+ */
+AudioDetails = MediaDetails.extend(/** @lends wp.media.view.AudioDetails.prototype */{
+	className: 'audio-details',
+	template:  wp.template('audio-details'),
+
+	setMedia: function() {
+		var audio = this.$('.wp-audio-shortcode');
+
+		if ( audio.find( 'source' ).length ) {
+			if ( audio.is(':hidden') ) {
+				audio.show();
+			}
+			this.media = MediaDetails.prepareSrc( audio.get(0) );
+		} else {
+			audio.hide();
+			this.media = false;
+		}
+
+		return this;
+	}
+});
+
+module.exports = AudioDetails;
+
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+var MediaDetails = wp.media.view.MediaDetails,
+	VideoDetails;
+
+/**
+ * wp.media.view.VideoDetails
+ *
+ * @memberOf wp.media.view
+ *
+ * @class
+ * @augments wp.media.view.MediaDetails
+ * @augments wp.media.view.Settings.AttachmentDisplay
+ * @augments wp.media.view.Settings
+ * @augments wp.media.View
+ * @augments wp.Backbone.View
+ * @augments Backbone.View
+ */
+VideoDetails = MediaDetails.extend(/** @lends wp.media.view.VideoDetails.prototype */{
+	className: 'video-details',
+	template:  wp.template('video-details'),
+
+	setMedia: function() {
+		var video = this.$('.wp-video-shortcode');
+
+		if ( video.find( 'source' ).length ) {
+			if ( video.is(':hidden') ) {
+				video.show();
+			}
+
+			if ( ! video.hasClass( 'youtube-video' ) && ! video.hasClass( 'vimeo-video' ) ) {
+				this.media = MediaDetails.prepareSrc( video.get(0) );
+			} else {
+				this.media = video.get(0);
+			}
+		} else {
+			video.hide();
+			this.media = false;
+		}
+
+		return this;
+	}
+});
+
+module.exports = VideoDetails;
+
+
+/***/ })
+/******/ ]);
