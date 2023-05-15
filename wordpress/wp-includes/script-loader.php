9fc800f75111c045ed9e88d10bcf269b023dcb62
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2023-05-15 08:01:29 +0000

    triggering build with pending triggers: 4; pending removals: 2

---
+++ wordpress/wp-includes/script-loader.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * WordPress scripts and styles default loader.
+ *
+ * Several constants are used to manage the loading, concatenating and compression of scripts and CSS:
+ * define('SCRIPT_DEBUG', true); loads the development (non-minified) versions of all scripts and CSS, and disables compression and concatenation,
+ * define('CONCATENATE_SCRIPTS', false); disables compression and concatenation of scripts and CSS,
+ * define('COMPRESS_SCRIPTS', false); disables compression of scripts,
+ * define('COMPRESS_CSS', false); disables compression of CSS,
+ * define('ENFORCE_GZIP', true); forces gzip for compression (default is deflate).
+ *
+ * The globals $concatenate_scripts, $compress_scripts and $compress_css can be set by plugins
+ * to temporarily override the above settings. Also a compression test is run once and the result is saved
+ * as option 'can_compress_scripts' (0/1). The test will run again if that option is deleted.
+ *
+ * @package WordPress
+ */
+
+/** WordPress Dependency Class */
+require( ABSPATH . WPINC . '/class-wp-dependency.php' );
+
+/** WordPress Dependencies Class */
+require( ABSPATH . WPINC . '/class.wp-dependencies.php' );
+
+/** WordPress Scripts Class */
+require( ABSPATH . WPINC . '/class.wp-scripts.php' );
+
+/** WordPress Scripts Functions */
+require( ABSPATH . WPINC . '/functions.wp-scripts.php' );
+
+/** WordPress Styles Class */
+require( ABSPATH . WPINC . '/class.wp-styles.php' );
+
+/** WordPress Styles Functions */
+require( ABSPATH . WPINC . '/functions.wp-styles.php' );
+
+/**
+ * Register all WordPress scripts.
+ *
+ * Localizes some of them.
+ * args order: `$scripts->add( 'handle', 'url', 'dependencies', 'query-string', 1 );`
+ * when last arg === 1 queues the script for the footer
+ *
+ * @since 2.6.0
+ *
+ * @param WP_Scripts $scripts WP_Scripts object.
+ */
+function wp_default_scripts( &$scripts ) {
+	include( ABSPATH . WPINC . '/version.php' ); // include an unmodified $wp_version
+
+	$develop_src = false !== strpos( $wp_version, '-src' );
+
+	if ( ! defined( 'SCRIPT_DEBUG' ) ) {
+		define( 'SCRIPT_DEBUG', $develop_src );
+	}
+
+	if ( ! $guessurl = site_url() ) {
+		$guessed_url = true;
+		$guessurl = wp_guess_url();
+	}
+
+	$scripts->base_url = $guessurl;
+	$scripts->content_url = defined('WP_CONTENT_URL')? WP_CONTENT_URL : '';
+	$scripts->default_version = get_bloginfo( 'version' );
+	$scripts->default_dirs = array('/wp-admin/js/', '/wp-includes/js/');
+
+	$suffix = SCRIPT_DEBUG ? '' : '.min';
+	$dev_suffix = $develop_src ? '' : '.min';
+
+	$scripts->add( 'utils', "/wp-includes/js/utils$suffix.js" );
+	did_action( 'init' ) && $scripts->localize( 'utils', 'userSettings', array(
+		'url' => (string) SITECOOKIEPATH,
+		'uid' => (string) get_current_user_id(),
+		'time' => (string) time(),
+		'secure' => (string) ( 'https' === parse_url( site_url(), PHP_URL_SCHEME ) ),
+	) );
+
+	$scripts->add( 'common', "/wp-admin/js/common$suffix.js", array('jquery', 'hoverIntent', 'utils'), false, 1 );
+	did_action( 'init' ) && $scripts->localize( 'common', 'commonL10n', array(
+		'warnDelete'   => __( "You are about to permanently delete these items from your site.\nThis action cannot be undone.\n 'Cancel' to stop, 'OK' to delete." ),
+		'dismiss'      => __( 'Dismiss this notice.' ),
+		'collapseMenu' => __( 'Collapse Main menu' ),
+		'expandMenu'   => __( 'Expand Main menu' ),
+	) );
+
+	$scripts->add( 'wp-a11y', "/wp-includes/js/wp-a11y$suffix.js", array( 'jquery' ), false, 1 );
+
+	$scripts->add( 'sack', "/wp-includes/js/tw-sack$suffix.js", array(), '1.6.1', 1 );
+
+	$scripts->add( 'quicktags', "/wp-includes/js/quicktags$suffix.js", array(), false, 1 );
+	did_action( 'init' ) && $scripts->localize( 'quicktags', 'quicktagsL10n', array(
+		'closeAllOpenTags'      => __( 'Close all open tags' ),
+		'closeTags'             => __( 'close tags' ),
+		'enterURL'              => __( 'Enter the URL' ),
+		'enterImageURL'         => __( 'Enter the URL of the image' ),
+		'enterImageDescription' => __( 'Enter a description of the image' ),
+		'textdirection'         => __( 'text direction' ),
+		'toggleTextdirection'   => __( 'Toggle Editor Text Direction' ),
+		'dfw'                   => __( 'Distraction-free writing mode' ),
+		'strong'          => __( 'Bold' ),
+		'strongClose'     => __( 'Close bold tag' ),
+		'em'              => __( 'Italic' ),
+		'emClose'         => __( 'Close italic tag' ),
+		'link'            => __( 'Insert link' ),
+		'blockquote'      => __( 'Blockquote' ),
+		'blockquoteClose' => __( 'Close blockquote tag' ),
+		'del'             => __
