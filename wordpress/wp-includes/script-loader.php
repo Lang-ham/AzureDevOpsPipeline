49c106647c40ee6433bfabea258f27d301f57171
Lang-ham <92838322+Lang-ham@users.noreply.github.com>
2023-05-15 16:59:24 +0000

    Run update-remote.sh on cassandra

--- wordpress/wp-includes/script-loader.php
+++ wordpress/wp-includes/script-loader.php
@@ -241,4 +241,175 @@ function wp_default_scripts( &$scripts ) {
 	did_action( 'init' ) && $scripts->localize( 'jquery-ui-autocomplete', 'uiAutocompleteL10n', array(
 		'noResults' => __( 'No results found.' ),
 		/* translators: Number of results found when using jQuery UI Autocomplete */
-		'oneResult' => __( '1 result found. Use up and down arrow keys to n
+		'oneResult' => __( '1 result found. Use up and down arrow keys to navigate.' ),
+		/* translators: %d: Number of results found when using jQuery UI Autocomplete */
+		'manyResults' => __( '%d results found. Use up and down arrow keys to navigate.' ),
+		'itemSelected' => __( 'Item selected.' ),
+	) );
+
+	// deprecated, not used in core, most functionality is included in jQuery 1.3
+	$scripts->add( 'jquery-form', "/wp-includes/js/jquery/jquery.form$suffix.js", array('jquery'), '4.2.1', 1 );
+
+	// jQuery plugins
+	$scripts->add( 'jquery-color', "/wp-includes/js/jquery/jquery.color.min.js", array('jquery'), '2.1.1', 1 );
+	$scripts->add( 'schedule', '/wp-includes/js/jquery/jquery.schedule.js', array('jquery'), '20m', 1 );
+	$scripts->add( 'jquery-query', "/wp-includes/js/jquery/jquery.query.js", array('jquery'), '2.1.7', 1 );
+	$scripts->add( 'jquery-serialize-object', "/wp-includes/js/jquery/jquery.serialize-object.js", array('jquery'), '0.2', 1 );
+	$scripts->add( 'jquery-hotkeys', "/wp-includes/js/jquery/jquery.hotkeys$suffix.js", array('jquery'), '0.0.2m', 1 );
+	$scripts->add( 'jquery-table-hotkeys', "/wp-includes/js/jquery/jquery.table-hotkeys$suffix.js", array('jquery', 'jquery-hotkeys'), false, 1 );
+	$scripts->add( 'jquery-touch-punch', "/wp-includes/js/jquery/jquery.ui.touch-punch.js", array('jquery-ui-widget', 'jquery-ui-mouse'), '0.2.2', 1 );
+
+	// Not used any more, registered for backwards compatibility.
+	$scripts->add( 'suggest', "/wp-includes/js/jquery/suggest$suffix.js", array('jquery'), '1.1-20110113', 1 );
+
+	// Masonry v2 depended on jQuery. v3 does not. The older jquery-masonry handle is a shiv.
+	// It sets jQuery as a dependency, as the theme may have been implicitly loading it this way.
+	$scripts->add( 'imagesloaded', "/wp-includes/js/imagesloaded.min.js", array(), '3.2.0', 1 );
+	$scripts->add( 'masonry', "/wp-includes/js/masonry.min.js", array( 'imagesloaded' ), '3.3.2', 1 );
+	$scripts->add( 'jquery-masonry', "/wp-includes/js/jquery/jquery.masonry$dev_suffix.js", array( 'jquery', 'masonry' ), '3.1.2b', 1 );
+
+	$scripts->add( 'thickbox', "/wp-includes/js/thickbox/thickbox.js", array('jquery'), '3.1-20121105', 1 );
+	did_action( 'init' ) && $scripts->localize( 'thickbox', 'thickboxL10n', array(
+		'next' => __('Next &gt;'),
+		'prev' => __('&lt; Prev'),
+		'image' => __('Image'),
+		'of' => __('of'),
+		'close' => __('Close'),
+		'noiframes' => __('This feature requires inline frames. You have iframes disabled or your browser does not support them.'),
+		'loadingAnimation' => includes_url('js/thickbox/loadingAnimation.gif'),
+	) );
+
+	$scripts->add( 'jcrop', "/wp-includes/js/jcrop/jquery.Jcrop.min.js", array('jquery'), '0.9.12');
+
+	$scripts->add( 'swfobject', "/wp-includes/js/swfobject.js", array(), '2.2-20120417');
+
+	// Error messages for Plupload.
+	$uploader_l10n = array(
+		'queue_limit_exceeded' => __('You have attempted to queue too many files.'),
+		'file_exceeds_size_limit' => __('%s exceeds the maximum upload size for this site.'),
+		'zero_byte_file' => __('This file is empty. Please try another.'),
+		'invalid_filetype' => __('Sorry, this file type is not permitted for security reasons.'),
+		'not_an_image' => __('This file is not an image. Please try another.'),
+		'image_memory_exceeded' => __('Memory exceeded. Please try another smaller file.'),
+		'image_dimensions_exceeded' => __('This is larger than the maximum size. Please try another.'),
+		'default_error' => __('An error occurred in the upload. Please try again later.'),
+		'missing_upload_url' => __('There was a configuration error. Please contact the server administrator.'),
+		'upload_limit_exceeded' => __('You may only upload 1 file.'),
+		'http_error' => __('HTTP error.'),
+		'upload_failed' => __('Upload failed.'),
+		/* translators: 1: Opening link tag, 2: Closing link tag */
+		'big_upload_failed' => __('Please try uploading this file with the %1$sbrowser uploader%2$s.'),
+		'big_upload_queued' => __('%s exceeds the maximum upload size for the multi-file uploader when used in your browser.'),
+		'io_error' => __('IO error.'),
+		'security_error' => __('Security error.'),
+		'file_cancelled' => __('File canceled.'),
+		'upload_stopped' => __('Upload stopped.'),
+		'dismiss' => __('Dismiss'),
+		'crunching' => __('Crunching&hellip;'),
+		'deleted' => __('moved to the trash.'),
+		'error_uploading' => __('&#8220;%s&#8221; has failed to upload.')
+	);
+
+	$scripts->add( 'moxiejs', "/wp-includes/js/plupload/moxie$suffix.js", array(), '1.3.5' );
+	$scripts->add( 'plupload', "/wp-includes/js/plupload/plupload$suffix.js", array( 'moxiejs' ), '2.1.9' );
+	// Back compat handles:
+	foreach ( array( 'all', 'html5', 'flash', 'silverlight', 'html4' ) as $handle ) {
+		$scripts->add( "plupload-$handle", false, array( 'plupload' ), '2.1.1' );
+	}
+
+	$scripts->add( 'plupload-handlers', "/wp-includes/js/plupload/handlers$suffix.js", array( 'plupload', 'jquery' ) );
+	did_action( 'init' ) && $scripts->localize( 'plupload-handlers', 'pluploadL10n', $uploader_l10n );
+
+	$scripts->add( 'wp-plupload', "/wp-includes/js/plupload/wp-plupload$suffix.js", array( 'plupload', 'jquery', 'json2', 'media-models' ), false, 1 );
+	did_action( 'init' ) && $scripts->localize( 'wp-plupload', 'pluploadL10n', $uploader_l10n );
+
+	// keep 'swfupload' for back-compat.
+	$scripts->add( 'swfupload', '/wp-includes/js/swfupload/swfupload.js', array(), '2201-20110113');
+	$scripts->add( 'swfupload-all', false, array( 'swfupload' ), '2201' );
+	$scripts->add( 'swfupload-handlers', "/wp-includes/js/swfupload/handlers$suffix.js", array('swfupload-all', 'jquery'), '2201-20110524');
+	did_action( 'init' ) && $scripts->localize( 'swfupload-handlers', 'swfuploadL10n', $uploader_l10n );
+
+	$scripts->add( 'comment-reply', "/wp-includes/js/comment-reply$suffix.js", array(), false, 1 );
+
+	$scripts->add( 'json2', "/wp-includes/js/json2$suffix.js", array(), '2015-05-03' );
+	did_action( 'init' ) && $scripts->add_data( 'json2', 'conditional', 'lt IE 8' );
+
+	$scripts->add( 'underscore', "/wp-includes/js/underscore$dev_suffix.js", array(), '1.8.3', 1 );
+	$scripts->add( 'backbone', "/wp-includes/js/backbone$dev_suffix.js", array( 'underscore','jquery' ), '1.2.3', 1 );
+
+	$scripts->add( 'wp-util', "/wp-includes/js/wp-util$suffix.js", array('underscore', 'jquery'), false, 1 );
+	did_action( 'init' ) && $scripts->localize( 'wp-util', '_wpUtilSettings', array(
+		'ajax' => array(
+			'url' => admin_url( 'admin-ajax.php', 'relative' ),
+		),
+	) );
+
+	$scripts->add( 'wp-sanitize', "/wp-includes/js/wp-sanitize$suffix.js", array('jquery'), false, 1 );
+
+	$scripts->add( 'wp-backbone', "/wp-includes/js/wp-backbone$suffix.js", array('backbone', 'wp-util'), false, 1 );
+
+	$scripts->add( 'revisions', "/wp-admin/js/revisions$suffix.js", array( 'wp-backbone', 'jquery-ui-slider', 'hoverIntent' ), false, 1 );
+
+	$scripts->add( 'imgareaselect', "/wp-includes/js/imgareaselect/jquery.imgareaselect$suffix.js", array('jquery'), false, 1 );
+
+	$scripts->add( 'mediaelement', false, array( 'jquery', 'mediaelement-core', 'mediaelement-migrate' ), '4.2.6-78496d1' );
+	$scripts->add( 'mediaelement-core', "/wp-includes/js/mediaelement/mediaelement-and-player$suffix.js", array(), '4.2.6-78496d1', 1 );
+	$scripts->add( 'mediaelement-migrate', "/wp-includes/js/mediaelement/mediaelement-migrate$suffix.js", array(), false, 1);
+
+	did_action( 'init' ) && $scripts->add_inline_script( 'mediaelement-core', sprintf( 'var mejsL10n = %s;', wp_json_encode( array(
+		'language' => strtolower( strtok( is_admin() ? get_user_locale() : get_locale(), '_-' ) ),
+		'strings'  => array(
+			'mejs.install-flash'       => __( 'You are using a browser that does not have Flash player enabled or installed. Please turn on your Flash player plugin or download the latest version from https://get.adobe.com/flashplayer/' ),
+			'mejs.fullscreen-off'      => __( 'Turn off Fullscreen' ),
+			'mejs.fullscreen-on'       => __( 'Go Fullscreen' ),
+			'mejs.download-video'      => __( 'Download Video' ),
+			'mejs.fullscreen'          => __( 'Fullscreen' ),
+			'mejs.time-jump-forward'   => array( __( 'Jump forward 1 second' ), __( 'Jump forward %1 seconds' ) ),
+			'mejs.loop'                => __( 'Toggle Loop' ),
+			'mejs.play'                => __( 'Play' ),
+			'mejs.pause'               => __( 'Pause' ),
+			'mejs.close'               => __( 'Close' ),
+			'mejs.time-slider'         => __( 'Time Slider' ),
+			'mejs.time-help-text'      => __( 'Use Left/Right Arrow keys to advance one second, Up/Down arrows to advance ten seconds.' ),
+			'mejs.time-skip-back'      => array( __( 'Skip back 1 second' ), __( 'Skip back %1 seconds' ) ),
+			'mejs.captions-subtitles'  => __( 'Captions/Subtitles' ),
+			'mejs.captions-chapters'   => __( 'Chapters' ),
+			'mejs.none'                => __( 'None' ),
+			'mejs.mute-toggle'         => __( 'Mute Toggle' ),
+			'mejs.volume-help-text'    => __( 'Use Up/Down Arrow keys to increase or decrease volume.' ),
+			'mejs.unmute'              => __( 'Unmute' ),
+			'mejs.mute'                => __( 'Mute' ),
+			'mejs.volume-slider'       => __( 'Volume Slider' ),
+			'mejs.video-player'        => __( 'Video Player' ),
+			'mejs.audio-player'        => __( 'Audio Player' ),
+			'mejs.ad-skip'             => __( 'Skip ad' ),
+			'mejs.ad-skip-info'        => array( __( 'Skip in 1 second' ), __( 'Skip in %1 seconds' ) ),
+			'mejs.source-chooser'      => __( 'Source Chooser' ),
+			'mejs.stop'                => __( 'Stop' ),
+			'mejs.speed-rate'          => __( 'Speed Rate' ),
+			'mejs.live-broadcast'      => __( 'Live Broadcast' ),
+			'mejs.afrikaans'           => __( 'Afrikaans' ),
+			'mejs.albanian'            => __( 'Albanian' ),
+			'mejs.arabic'              => __( 'Arabic' ),
+			'mejs.belarusian'          => __( 'Belarusian' ),
+			'mejs.bulgarian'           => __( 'Bulgarian' ),
+			'mejs.catalan'             => __( 'Catalan' ),
+			'mejs.chinese'             => __( 'Chinese' ),
+			'mejs.chinese-simplified'  => __( 'Chinese (Simplified)' ),
+			'mejs.chinese-traditional' => __( 'Chinese (Traditional)' ),
+			'mejs.croatian'            => __( 'Croatian' ),
+			'mejs.czech'               => __( 'Czech' ),
+			'mejs.danish'              => __( 'Danish' ),
+			'mejs.dutch'               => __( 'Dutch' ),
+			'mejs.english'             => __( 'English' ),
+			'mejs.estonian'            => __( 'Estonian' ),
+			'mejs.filipino'            => __( 'Filipino' ),
+			'mejs.finnish'             => __( 'Finnish' ),
+			'mejs.french'              => __( 'French' ),
+			'mejs.galician'            => __( 'Galician' ),
+			'mejs.german'              => __( 'German' ),
+			'mejs.greek'               => __( 'Greek' ),
+			'mejs.haitian-creole'      => __( 'Haitian Creole' ),
+			'mejs.hebrew'              => __( 'Hebrew' ),
+			'mejs.hindi'               => __( 'Hindi' ),
+		
